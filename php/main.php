f35c75330dad905703e477eba8cb18957c944a55
Saoki11111 <t.aoki11111@gmail.com>
2020-05-14 01:45:28 +0900

    ~ Lesson 6

--- php/main.php
+++ php/main.php
@@ -1,6 +1,12 @@
 <?php
 
-echo 'Hello taguchi' . PHP_EOL;
-echo 'Hi taguchi' . PHP_EOL;
+// $name = 'taguchi';
+$name = 'dotinstall';
+
+// echo 'Hello taguchi' . PHP_EOL;
+// echo 'Hi taguchi' . PHP_EOL;
+
+echo "Hello $name \" $name \" . \t $name" . PHP_EOL;
+echo 'Hi $name \"$name\"' . $name . PHP_EOL;
 
 /* ?> は HTML を書かないので不要 */
